7284a4b6295ac22be104a1b0aa0d665c1522eb2f
Adam Hladik <adi.hladik@gmail.com>
2023-08-17 10:55:44 +0200

    change: relay renamed to ctx

--- mod.ts
+++ mod.ts
@@ -6,26 +6,26 @@ import { createExplorer } from './static.ts'
 export * from './util.ts'
 export { h, Fragment } from 'https://deno.land/x/jsx@v0.1.5/mod.ts'
 
-type Relay = Record<string, unknown>
+type Context = Record<string, unknown>
 export type PageOptions = {
   title: string
   lang: string
   head: Array<Node<unknown>>
 }
-export type Props<T extends Relay = Relay> = {
+export type Props<T extends Context = Context> = {
   request: Request
   params: Record<string, string>
   query: URLSearchParams
-  relay: T
+  ctx: T
   headers: Headers
   children: Array<Node<unknown>>
   url: URL
   page: PageOptions
 }
 type RouteResponse = Response | Node<unknown> | Promise<Response> | Promise<Node<unknown>> | void | Promise<void>
-type Route<T extends Relay> = ((input: Props<T>) => RouteResponse) | Component<T>
-type Component<T extends Relay> = (props: Props<T>) => Node<unknown> | Promise<Node<unknown>>
-export type Routes<T extends Relay = Relay> = Record<string | symbol, Route<T>>
+type Route<T extends Context> = ((input: Props<T>) => RouteResponse) | Component<T>
+type Component<T extends Context> = (props: Props<T>) => Node<unknown> | Promise<Node<unknown>>
+export type Routes<T extends Context = Context> = Record<string | symbol, Route<T>>
 export type Config = {
   static?: string
   unoCSS?: boolean
@@ -34,14 +34,14 @@ export type Config = {
 export const NOT_FOUND = Symbol('Page not found')
 export const MIDDLEWARE = Symbol('Middleware')
 
-const defaultRoutes: Routes<Relay> = {
+const defaultRoutes: Routes<Context> = {
   [NOT_FOUND]: (props) => new Response(`Not found\n${props.request.url}`, {
     status: 404,
   }),
   [MIDDLEWARE]: () => {},
 }
 
-export function withLayout<T extends Relay>(Layout: Component<T>, Target: Component<T>) {
+export function withLayout<T extends Context>(Layout: Component<T>, Target: Component<T>) {
   return function WrappedComponent(props: Props<T>) {
     return {
       type: Layout,
@@ -56,7 +56,7 @@ export function withLayout<T extends Relay>(Layout: Component<T>, Target: Compon
   }
 }
 
-export async function createHandler<T extends Relay>(routes: Routes<T>, config: Config = {}) {
+export async function createHandler<T extends Context>(routes: Routes<T>, config: Config = {}) {
   const match = createMatcher(routes)
   const getStaticFile = await createExplorer(config.static)
 
@@ -82,7 +82,7 @@ export async function createHandler<T extends Relay>(routes: Routes<T>, config: 
       children: [],
       params: {},
       query: new URLSearchParams(url.search),
-      relay: {} as T,
+      ctx: {} as T,
       headers: new Headers(),
     } as Props<T>
 
